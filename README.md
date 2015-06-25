d1e109de02615a20c61c9622bf1d071f6807977f
coderaiser <mnemonic.enemy@gmail.com>
2015-06-25 13:23:01 -0400

    feature(remedy) add

---
+++ README.md
@@ -0,0 +1,82 @@
+# Remedy
+
+File remove emitter middleware based on [socket.io](http://socket.io "Socket.io") and [remy](https://github.com/coderaiser/node-remy "Remy").
+
+## Install
+
+```
+npm i remedy --save
+```
+
+## Client
+
+Could be loaded from url `/remedy/remedy.js`.
+
+```js
+/* could be one argument: callback */
+remedy('/remedy', function() {
+    var from        = '/',
+        to          = '/tmp',
+        names       = [
+            'bin'
+        ],
+        progress    = function(value) {
+            console.log('progress:', value);
+        },
+        
+        end     = function() {
+            console.log('end');
+            remedy.removeListener('progress', progress);
+            remedy.removeListener('end', end);
+        },
+    
+    error   = function(data) {
+        var msg = data + '\n Continue?',
+            is = confirm(msg);
+        
+        if (is)
+            remedy.continue();
+        else
+            remedy.abort();
+    };
+    
+    remedy(from, names);
+    
+    remedy.on('progress', progress);
+    remedy.on('end', end);
+    remedy.on('error', error);
+});
+
+```
+
+## Server
+
+```js
+var remedy      = require('remedy'),
+    http        = require('http'),
+    express     = require('express'),
+    io          = require('socket.io'),
+    app         = express(),
+    server      = http.createServer(app);
+
+socket          = io.listen(server),
+server.listen(port, ip);
+
+app.use(remedy({
+    minify: true,
+    online: true
+});
+
+remedy.listen(socket, {
+    prefix: '/remedy',  /* default              */
+    root: '/',          /* string or function   */
+});
+```
+
+```js
+
+```
+
+## License
+
+MIT
---
+++ js/remedy.js
@@ -0,0 +1,188 @@
+var io, load, exec, join, Emitify, loadRemote;
+
+(function(global) {
+    'use strict';
+    
+    if (typeof module !== 'undefined' && module.exports)
+        module.exports  = new RemedyProto();
+    else
+        global.remedy   = new RemedyProto();
+    
+    function RemedyProto() {
+        var Progress;
+        
+        function Remedy(prefix, callback) {
+            if (!callback) {
+                callback    = prefix;
+                prefix      = '/remedy';
+            }
+            
+            loadAll(prefix, function() {
+                Progress                = new ProgressProto(prefix);
+                Object.setPrototypeOf(Remedy, Emitify);
+                
+                if (typeof callback === 'function')
+                    callback();
+            });
+        }
+        
+        Remedy.copy  = function(from, to, files) {
+            Progress.copy(from, to, files);
+        };
+        
+        Remedy.abort = function() {
+            Progress.abort();
+        };
+        
+        Remedy.pause = function() {
+            Progress.pause();
+        };
+        
+        Remedy.continue = function() {
+            Progress.continue();
+        };
+        
+        function loadAll(prefix, callback) {
+            var scripts = [];
+            
+            if (!exec)
+                scripts.push('/modules/execon/lib/exec.js');
+            
+            if (!scripts.length)
+                loadFiles(prefix, callback);
+            else
+                loadScript(scripts.map(function(name) {
+                    return prefix + name;
+                }), function() {
+                    loadFiles(prefix, callback);
+                }); 
+        }
+        
+        function loadFiles(prefix, callback) {
+            exec.series([
+                function(callback) {
+                    var scripts = [
+                        '/js/loadremote.js'
+                    ];
+                    
+                    if (!load)
+                        scripts.push('/modules/load/load.js');
+                    
+                    if (!join)
+                        scripts.push('/join/join.js');
+                    
+                    if (!Emitify)
+                        scripts.push('/modules/emitify/lib/emitify.js');
+                    
+                    scripts = scripts.map(function(name) {
+                        return prefix + name;
+                    });
+                    
+                    exec.if(!scripts.length, callback, function() {
+                        loadScript(scripts, callback);
+                    });
+                },
+                
+                function(callback) {
+                    loadRemote('socket', {
+                        name : 'io',
+                        prefix: prefix,
+                        noPrefix: true
+                    }, callback);
+                },
+                
+                function() {
+                    callback();
+                }
+            ]);
+        }
+        
+        function loadScript(srcs, callback) {
+            var i       = srcs.length,
+                func    = function() {
+                    --i;
+                    
+                    if (!i)
+                        callback();
+                };
+            
+            srcs.forEach(function(src) {
+                var element = document.createElement('script');
+                
+                element.src = src;
+                element.addEventListener('load', function load() {
+                    func();
+                    element.removeEventListener('load', load);
+                });
+                
+                document.body.appendChild(element);
+            });
+        }
+        
+        function ProgressProto(room) {
+            var socket;
+            
+            init(room);
+            
+            function init(room) {
+                var href            = getHost(),
+                    FIVE_SECONDS    = 5000;
+                
+                socket = io.connect(href + room, {
+                    'max reconnection attempts' : Math.pow(2, 32),
+                    'reconnection limit'        : FIVE_SECONDS
+                });
+                
+                socket.on('err', function(error) {
+                    Remedy.emit('error', error);
+                });
+                
+                socket.on('file', function(name) {
+                    Remedy.emit('file', name);
+                });
+                
+                socket.on('progress', function(percent) {
+                    Remedy.emit('progress', percent);
+                });
+                
+                socket.on('end', function() {
+                    Remedy.emit('end');
+                });
+                
+                socket.on('connect', function() {
+                    Remedy.emit('connect');
+                });
+                
+                socket.on('disconnect', function() {
+                    Remedy.emit('disconnect');
+                });
+            }
+            
+            this.pause       = function() {
+                socket.emit('pause');
+            };
+            
+            this.continue   = function() {
+                socket.emit('continue');
+            };
+            
+            this.abort      = function() {
+                socket.emit('abort');
+            };
+            
+            this.remove     = function(from, files) {
+                socket.emit('remove', from, files);
+            };
+            
+            function getHost() {
+                var l       = location,
+                    href    = l.origin || l.protocol + '//' + l.host;
+                
+                return href;
+            }
+        }
+        
+        return Remedy;
+    }
+    
+})(this);
